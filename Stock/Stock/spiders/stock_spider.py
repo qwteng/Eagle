21aafa109b50612ea7820ff080d1a645669bc53a
qwteng <qwteng@qq.com>
2016-04-16 21:58:45 +0800

    multi level links process

--- Stock/Stock/spiders/stock_spider.py
+++ Stock/Stock/spiders/stock_spider.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
 from scrapy import Spider
 from scrapy.selector import Selector
 
@@ -10,12 +13,35 @@ class StockSpider(Spider):
 		"http://quote.eastmoney.com/stocklist.html"
 	]
 
-	def parse(self, response):
-		links = Selector(response).xpath('//ul/li')
-		for  sel in links:
-			item = StockItem()
-			name = sel.xpath('a/text()').extract()
-			item['link'] = name
-			
-			yield item
+	def is_shsz_stocklink(self, link):
+		elems = link.split('.')
+		if len(elems) != 4:
+			return False
+		if len(elems[2]) != 12:
+			return False
+		code = elems[2][-6:]
 
+		return ("30000" in code)
+
+
+	def parse(self, response):
+		for  sel in response.xpath('//ul/li'):
+			link = sel.xpath('a/@href').extract()
+			if len(link) == 0 :
+				continue
+			url = link[0]
+			if self.is_shsz_stocklink(url):
+				yield scrapy.Request(url, callback=self.parse_stock_page)
+
+	def parse_stock_page(self, response):
+		for sel in response.xpath('//div[@class="cells"]'):
+			links = sel.xpath(".//a")
+			for l in links:
+				name = l.xpath('text()').extract()
+				if name[0] == u'股东研究':
+					url_holders= l.xpath('@href').extract()
+					yield scrapy.Request(url_holders[0], callback=self.parse_shareholderresearh)
+
+	def parse_shareholderresearh(self, response):
+		for sel in response.xpath('//div[@id="TTS_Table_Div"]'):
+			print sel
